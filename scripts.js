18fbb5f6b110f158444be31f28687b927cf28b63
Nick de Groot <Nickdgg@gmail.com>
2021-06-24 14:25:06 +0200

    Delete btn now works plus some other minor adjustments

--- scripts.js
+++ scripts.js
@@ -3,38 +3,69 @@ const inputOverlay = document.querySelector('#overlay');
 const submitBookButton = document.querySelector('#submit-btn');
 const addBookButton = document.querySelector('#add-book-btn');
 const inputForm = document.querySelector('.book-input-container');
+const inputFormCancelButton = document.querySelector('#input-cross-btn');
 
 let myLibrary = [];
 
 /* EVENT LISTENERS */
 
+// Show input form upon clicking 'Add Book'
 addBookButton.addEventListener('click', showInput);
 
+// Hide input form and delete current input upon clicking 'Cancel Input Cross btn'
+inputFormCancelButton.addEventListener('click', () => {
+	resetFormInput();
+	hideInput();
+});
+
+//Create new book object and display it upon clicking submit
 submitBookButton.addEventListener('click', () => {
-	const bookName = document.querySelector('#btitle').value;
-	const bookAuthor = document.querySelector('#bauthor').value;
-	const bookPages = document.querySelector('#bpages').value;
-	const bookProgress = document.querySelector('#bprogress').value;
+	let bookTitle = document.querySelector('#btitle').value;
+	let bookAuthor = document.querySelector('#bauthor').value;
+	let bookPages = document.querySelector('#bpages').value;
+	let bookProgress = document.querySelector('#bprogress').value;
+	let ID = createID();
+
 	const isEmpty = (str) => !str.trim().length;
-	if (isEmpty(bookName) || isEmpty(bookAuthor) || isEmpty(bookPages)) {
+	if (isEmpty(bookTitle) || isEmpty(bookAuthor) || isEmpty(bookPages)) {
 		inputForm.classList.add('invalid-input');
 		setTimeout(function () {
 			inputForm.classList.remove('invalid-input');
 		}, 1000);
 		return;
 	}
-	let currentBook = new Book(bookName, bookAuthor, bookPages, bookProgress);
+
+	let currentBook = new Book(
+		bookTitle,
+		bookAuthor,
+		bookPages,
+		bookProgress,
+		ID
+	);
 	addBookToLibrary(currentBook);
 	clearBooks();
 	createBook();
+	resetFormInput();
 	hideInput();
 });
 
-function resetInputForm() {
-	bookName = '';
-	bookAuthor = '';
-	bookPages = '';
-	bookProgress = '';
+/* FUNCTIONS */
+
+function createID() {
+	let ID;
+	if (myLibrary.length == 0) {
+		ID = 1;
+	} else {
+		ID = myLibrary[myLibrary.length - 1].id + 1;
+	}
+	return ID;
+}
+
+function resetFormInput() {
+	document.querySelector('#btitle').value = '';
+	document.querySelector('#bauthor').value = '';
+	document.querySelector('#bpages').value = '';
+	document.querySelector('#bprogress').value = 'notread';
 }
 
 function showInput() {
@@ -45,11 +76,12 @@ function hideInput() {
 	inputOverlay.style.display = 'none';
 }
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
+	this.id = id;
 }
 
 function clearBooks() {
@@ -63,11 +95,32 @@ function addBookToLibrary(book) {
 	myLibrary.push(book);
 }
 
+function deleteCurrentBook(currentBook) {
+	var currentBookID = currentBook.target.parentNode.parentNode.id;
+	if (currentBookID == 'edit-book-btn') {
+		currentBookID = currentBook.target.parentNode.parentNode.parentNode.id;
+	}
+	myLibrary.forEach((book) => {
+		if (book.id == currentBookID) {
+			myLibrary.splice(myLibrary.indexOf(book), 1);
+		}
+		clearBooks();
+		createBook();
+	});
+}
+
 function createBook() {
 	myLibrary.forEach((book) => {
 		const newBook = document.createElement('div');
 		newBook.classList = 'book_showcase--book';
+		newBook.setAttribute('id', `${book.id}`);
 		bookContainer.prepend(newBook);
+
+		const editBtn = document.createElement('span');
+		editBtn.setAttribute('id', 'edit-book-btn');
+		editBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M9.036 7.976a.75.75 0 00-1.06 1.06L10.939 12l-2.963 2.963a.75.75 0 101.06 1.06L12 13.06l2.963 2.964a.75.75 0 001.061-1.06L13.061 12l2.963-2.964a.75.75 0 10-1.06-1.06L12 10.939 9.036 7.976z"></path><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"></path></svg>`;
+		newBook.appendChild(editBtn);
+		editBtn.addEventListener('click', deleteCurrentBook);
 
 		const bookTitle = document.createElement('h3');
 		bookTitle.classList = 'book-title';
@@ -79,6 +132,12 @@ function createBook() {
 		newBook.appendChild(bookAuthor);
 		bookAuthor.textContent = book.author;
 
+		const bookIcon = document.createElement('span');
+		bookIcon.setAttribute('id', 'book-icon');
+		bookIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M404.72,82.944c-0.027,0-0.054,0-0.08,0h0h-27.12v-9.28c0.146-3.673-2.23-6.974-5.76-8    c-18.828-4.934-38.216-7.408-57.68-7.36c-32,0-75.6,7.2-107.84,40c-32-33.12-75.92-40-107.84-40    c-19.464-0.048-38.852,2.426-57.68,7.36c-3.53,1.026-5.906,4.327-5.76,8v9.2H8c-4.418,0-8,3.582-8,8v255.52c0,4.418,3.582,8,8,8    c1.374-0.004,2.724-0.362,3.92-1.04c0.8-0.4,80.8-44.16,192.48-16h1.2h0.72c0.638,0.077,1.282,0.077,1.92,0    c112-28.4,192,15.28,192.48,16c2.475,1.429,5.525,1.429,8,0c2.46-1.42,3.983-4.039,4-6.88V90.944    C412.72,86.526,409.139,82.944,404.72,82.944z M16,333.664V98.944h19.12v200.64c-0.05,4.418,3.491,8.04,7.909,8.09    c0.432,0.005,0.864-0.025,1.291-0.09c16.55-2.527,33.259-3.864,50-4c23.19-0.402,46.283,3.086,68.32,10.32    C112.875,307.886,62.397,314.688,16,333.664z M94.32,287.664c-14.551,0.033-29.085,0.968-43.52,2.8V79.984    c15.576-3.47,31.482-5.241,47.44-5.28c29.92,0,71.2,6.88,99.84,39.2l0.24,199.28C181.68,302.304,149.2,287.664,94.32,287.664z     M214.32,113.904c28.64-32,69.92-39.2,99.84-39.2c15.957,0.047,31.863,1.817,47.44,5.28v210.48    c-14.354-1.849-28.808-2.811-43.28-2.88c-54.56,0-87.12,14.64-104,25.52V113.904z M396.64,333.664    c-46.496-19.028-97.09-25.831-146.96-19.76c22.141-7.26,45.344-10.749,68.64-10.32c16.846,0.094,33.663,1.404,50.32,3.92    c4.368,0.663,8.447-2.341,9.11-6.709c0.065-0.427,0.095-0.859,0.09-1.291V98.944h19.12L396.64,333.664z"></path></svg>`;
+		newBook.appendChild(bookIcon);
+		bookIcon.addEventListener('click', deleteCurrentBook);
+
 		const bookPages = document.createElement('h5');
 		bookPages.classList = 'book-pages';
 		newBook.appendChild(bookPages);
@@ -87,9 +146,11 @@ function createBook() {
 		const isRead = document.createElement('div');
 		isRead.classList = 'book-read';
 		newBook.appendChild(isRead);
-		if (book.read) {
+		if (book.read == 'finished') {
 			isRead.textContent = 'Finished';
-		} else {
+		} else if (book.read == 'reading') {
+			isRead.textContent = 'Reading';
+		} else if (book.read == 'notread') {
 			isRead.textContent = 'Not Read';
 		}
 	});
